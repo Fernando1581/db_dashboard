8058b51b073d143371154c7c1f73c0dc58309bdd
Fernando1581 <l.fernando.557535@umindanao.edu.ph>
2026-02-23 21:07:36 +0800

    Updated Files and with new Pages

--- index.php
+++ index.php
@@ -17,22 +17,47 @@ $revenue = $revRow['s'];
 </head>
 <body>
 <?php include "nav.php"; ?>
- 
-<div class= "container">
   <h2>Dashboard</h2>
-  
-  <ul>
-    <li>Total Clients: <b><?php echo $clients; ?></b></li>
-    <li>Total Services: <b><?php echo $services; ?></b></li>
-    <li>Total Bookings: <b><?php echo $bookings; ?></b></li>
-    <li>Total Revenue: <b>₱<?php echo number_format($revenue,2); ?></b></li>
-  </ul>
+
+  <div class="container">
+    
+    <div class="card">
+      <h3>Total Clients</h3>
+      <p><?php echo $clients; ?></p>
+    </div>
+
+    <div class="card">
+      <h3>Total Services</h3>
+      <p><?php echo $services; ?></p>
+    </div>
+
+    <div class="card">
+      <h3>Total Bookings</h3>
+      <p><?php echo $bookings; ?></p>
+    </div>
+
+    <div class="card">
+      <h3>Total Revenue</h3>
+      <p>₱<?php echo number_format($revenue,2); ?></p>
+    </div>
+
+  </div>
+
+<div class="quick_links">
+
+  <div class="quick_buttons">
+    <a href="/assessment_beginner/pages/clients_add.php" class="quick_btn">
+      Add Client
+    </a>
+
+    <a href="/assessment_beginner/pages/bookings_create.php" class="quick_btn">
+      Create Booking
+    </a>
+  </div>
+
+  <p class="quick_title">Quick Links</p>
+
 </div>
-<p>
-  Quick links:
-  <a href="/assessment_beginner/pages/clients_add.php">Add Client</a> |
-  <a href="/assessment_beginner/pages/bookings_create.php">Create Booking</a>
-</p>
  
 </body>
 </html>
---
+++ pages/clients_add.php
@@ -0,0 +1,54 @@
+<?php
+include "../db.php";
+ 
+$message = "";
+ 
+if (isset($_POST['save'])) {
+  $full_name = $_POST['full_name'];
+  $email = $_POST['email'];
+  $phone = $_POST['phone'];
+  $address = $_POST['address'];
+ 
+  if ($full_name == "" || $email == "") {
+    $message = "Name and Email are required!";
+  } else {
+    $sql = "INSERT INTO clients (full_name, email, phone, address)
+            VALUES ('$full_name', '$email', '$phone', '$address')";
+    mysqli_query($conn, $sql);
+    header("Location: clients_list.php");
+    exit;
+  }
+}
+?>
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8"><title>Add Client</title>
+  <link rel="stylesheet" href="/assessment_beginner/style.css">
+</head>
+<body>
+<?php include "../nav.php"; ?>
+ 
+<div class="add_client">
+  <h2>Add Client</h2>
+  <p style="color:red;"><?php echo $message; ?></p>
+    
+  <form method="post">
+    <label>Full Name*</label>
+    <input type="text" name="full_name">
+    
+    <label>Email*</label>
+    <input type="text" name="email">
+    
+    <label>Phone</label>
+    <input type="text" name="phone">
+    
+    <label>Address</label>
+    <input type="text" name="address">
+    
+    <button type="submit" name="save">Save</button>
+  </form>
+</div>
+
+</body>
+</html>
